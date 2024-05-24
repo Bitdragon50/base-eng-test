0c158d74b285dc5c5e6d2b8cfd2fd6077369beb0
bitdragon50 <boseme@hotmail.co.uk>
2024-05-24 07:46:52 +0100

    syn fix

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -37,7 +37,7 @@ variable "timeout" {
 variable "function_name" {
   description = "The name of the Lambda Function."
   type        = string
-  default     = "chatting-api"
+  default     = "chatting_api"
 }
 
 variable "function_handler" {
